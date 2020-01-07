a8f25952547c5bea227709805087ac011a984c46
Subhra <sb00479570@techmahindra.com>
2020-01-07 12:34:42 -0500

    nav import removed from components

--- container/Login.js
+++ container/Login.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button, Image, TouchableOpacity, TextInput } from 'react-native';
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from 'react-navigation-stack';
 
 import Toolbar from '../components/Toolbar';
 
