5cf8bdb0b65fc67ce36d961929f2b14899f736b7
Subhra <sb00479570@techmahindra.com>
2020-01-09 16:40:23 -0500

    fix

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ const AppNavigator = createStackNavigator({
     defaultNavigationOptions: {
       headerShown: false
     },
-    initialRouteName: "Dashboard"
+    initialRouteName: "Home"
   });
 
 const AppContainer = createAppContainer(AppNavigator);
